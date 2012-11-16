7418ff9f5b562ef4934f6ffc474bc6e1c4859b73
jonerez <jonerez@gmail.com>
2012-11-16 17:00:32 +0000

    Some cleaning up in Client

--- choosie-android/ChoosieApp/src/com/choosie/app/ChoosieClient.java
+++ choosie-android/ChoosieApp/src/com/choosie/app/ChoosieClient.java
@@ -27,21 +27,102 @@ import org.json.JSONObject;
 import android.content.Context;
 import android.graphics.Bitmap.CompressFormat;
 import android.os.AsyncTask;
+import android.os.Message;
 
 public class ChoosieClient {
 
-	void sendChoosiePostToServer(NewChoosiePostData data) {
+	/**
+	 * Gets a ChoosiePost (photo1, photo2 and a question) and posts it to the server.
+	 * @param data
+	 */
+	public void sendChoosiePostToServer(NewChoosiePostData data) {
 		final HttpClient httpClient = new DefaultHttpClient();
-		AsyncTask<HttpPost, Void, HttpResponse> executeHttpPostTask = getExecuteHttpPostTask(httpClient);
-
+		
 		HttpPost postRequest = null;
 		try {
+			// Creates the POST request
 			postRequest = createHttpPostRequest(data);
-			executeHttpPostTask.execute(postRequest);
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		if (postRequest == null) {
+			return;
+		}
+		
+		// Executes the POST request, async
+		AsyncTask<HttpPost, Void, HttpResponse> executeHttpPostTask = getExecuteHttpPostTask(httpClient);
+
+		executeHttpPostTask.execute(postRequest);
+	}
+	
+	/**
+	 * Represents the data that is returned from the server.
+	 */
+	public class ChoosiePostData {
+		String photo1URL;
+		String photo2URL;
+		String question;
+	}
+
+	/**
+	 * Gets the feed from the server.
+	 * Calls the callback when the feed is back.
+	 */
+	void getFeedFromServer(Callback<List<ChoosiePostData>> callback) {
+		final HttpClient client = new DefaultHttpClient();
+
+		// Creates the GET HTTP request
+		final HttpGet request = new HttpGet(
+				"http://choosieapp.appspot.com/feed");
+
+		// Executes the GET request async
+		AsyncTask<HttpGet, Void, String> getStreamTask = createGetStreamTask(client, callback);
+		getStreamTask.execute(request);
+	}
+
+	private AsyncTask<HttpGet, Void, String> createGetStreamTask(
+			final HttpClient client, final Callback<List<ChoosiePostData>> callback) {
+		AsyncTask<HttpGet, Void, String> getStreamTask = new AsyncTask<HttpGet, Void, String>() {
+
+			@Override
+			protected String doInBackground(HttpGet... params) {
+				HttpResponse response;
+				try {
+					response = client.execute(params[0]);
+					return EntityUtils.toString(response.getEntity());
+				} catch (ClientProtocolException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return null;
+			}
+
+			@Override
+			protected void onPostExecute(String jsonString) {
+				if (jsonString == null) {
+					// TODO Handle error
+					return;
+				}
+				try {
+					List<ChoosiePostData> choosiePostsFromFeed = convertJsonToChoosiePosts(jsonString);
+
+					callback.handleOperationFinished(choosiePostsFromFeed);
+				} catch (JSONException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+
+		};
+		return getStreamTask;
+	}
+
+	void getPictureFromServer() {
+
 	}
 
 	private AsyncTask<HttpPost, Void, HttpResponse> getExecuteHttpPostTask(
@@ -66,88 +147,32 @@ public class ChoosieClient {
 		return executeHttpPostTask;
 	}
 
-	public class ChoosiePostData {
-		String photo1URL;
-		String photo2URL;
-		String question;
-	}
-
-	void getFeedFromServer() {
-		final HttpClient client = new DefaultHttpClient();
-		final HttpGet request = new HttpGet(
-				"http://choosieapp.appspot.com/feed");
-
-		AsyncTask<Context, Void, String> getStreamTask = new AsyncTask<Context, Void, String>() {
-
-			@Override
-			protected String doInBackground(Context... params) {
-				HttpResponse response;
-				try {
-					response = client.execute(request);
-					return EntityUtils.toString(response.getEntity());
-				} catch (ClientProtocolException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				return null;
+	/**
+	 * Takes a JSON string, and builds ChoosiePostData objet from it.
+	 * 
+	 * @param jsonString
+	 * @return
+	 * @throws JSONException
+	 */
+	private List<ChoosiePostData> convertJsonToChoosiePosts(String jsonString)
+			throws JSONException {
+		JSONObject jsonObject = new JSONObject(jsonString);
+		JSONArray jsonArray = jsonObject.getJSONArray("feed");
+		List<ChoosiePostData> choosiePostsFromFeed = new ArrayList<ChoosieClient.ChoosiePostData>();
+		for (int i = 0; i < jsonArray.length(); i++) {
+			try {
+				JSONObject singleItemJsonObject = jsonArray.getJSONObject(i);
+				ChoosiePostData postData = new ChoosiePostData();
+				postData.photo1URL = singleItemJsonObject.getString("photo1");
+				postData.photo2URL = singleItemJsonObject.getString("photo2");
+				postData.question = singleItemJsonObject.getString("question");
+				choosiePostsFromFeed.add(postData);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-
-			@Override
-			protected void onPostExecute(String jsonString) {
-				if (jsonString == null) {
-					// TODO Handle error
-					return;
-				}
-				try {
-					List<ChoosiePostData> choosiePostsFromFeed = convertJsonToChoosiePosts(jsonString);
-
-					choosiePostsFromFeed = choosiePostsFromFeed;
-				} catch (JSONException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-
-			/**
-			 * Takes a JSON string, and builds ChoosiePostData objet from it.
-			 * 
-			 * @param jsonString
-			 * @return
-			 * @throws JSONException
-			 */
-			private List<ChoosiePostData> convertJsonToChoosiePosts(
-					String jsonString) throws JSONException {
-				JSONObject jsonObject = new JSONObject(jsonString);
-				JSONArray jsonArray = jsonObject.getJSONArray("feed");
-				List<ChoosiePostData> choosiePostsFromFeed = new ArrayList<ChoosieClient.ChoosiePostData>();
-				for (int i = 0; i < jsonArray.length(); i++) {
-					try {
-						JSONObject singleItemJsonObject = jsonArray
-								.getJSONObject(i);
-						ChoosiePostData postData = new ChoosiePostData();
-						postData.photo1URL = singleItemJsonObject
-								.getString("photo1");
-						postData.photo2URL = singleItemJsonObject
-								.getString("photo2");
-						postData.question = singleItemJsonObject
-								.getString("question");
-						choosiePostsFromFeed.add(postData);
-					} catch (JSONException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				return choosiePostsFromFeed;
-			}
-		};
-		getStreamTask.execute();
-	}
-
-	void getPictureFromServer() {
-
+		}
+		return choosiePostsFromFeed;
 	}
 
 	private HttpPost createHttpPostRequest(NewChoosiePostData data)
