e9c1548b2b82b107f05f7018c2692922ef30be76
klinger.idan@gmail.com <klinger.idan@gmail.com@6f50cedd-f6cd-5b0e-d468-1939c3eac22f>
2012-11-24 14:03:33 +0000

    add Login button for re-entering application

--- trunk/choosie-android/ChoosieApp/src/com/choosie/app/StartActivity.java
+++ trunk/choosie-android/ChoosieApp/src/com/choosie/app/StartActivity.java
@@ -11,7 +11,10 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.sax.TextElementListener;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,6 +30,17 @@ public class StartActivity extends FacebookActivity {
 		welcome.setText(getResources().getString(R.string.welcome) + " "
 				+ getResources().getString(R.string.app_name) + "!");
 
+		Button fbLoginButton = (Button)findViewById(R.id.fbLoginButton);
+		fbLoginButton.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Log.i(Constants.LOG_TAG, "on click open session");
+				closeSession();
+				openSession();
+				
+			}
+		});
+		
 		this.openSession();
 	}
 
@@ -39,6 +53,7 @@ public class StartActivity extends FacebookActivity {
 	@Override
 	protected void onSessionStateChange(SessionState state, Exception exception) {
 		// user has either logged in or not ...
+		Log.i(Constants.LOG_TAG, "onSessionStateChange");
 		if (state.isOpened()) {
 			// make request to the /me API
 			Request request = Request.newMeRequest(this.getSession(),
@@ -46,10 +61,12 @@ public class StartActivity extends FacebookActivity {
 
 						public void onCompleted(GraphUser user,
 								Response response) {
+							Log.i(Constants.LOG_TAG, "onCompleted");
 							if (user != null) {
 								TextView welcome = (TextView) findViewById(R.id.welcome);
 								welcome.setText("Hello " + user.getName() + "!");
 
+								Log.i(Constants.LOG_TAG, "creating intent for ChoosieActivity");	
 								Intent intent = new Intent(StartActivity.this,
 										ChoosieActivity.class);
 								FacebookDetails details = new FacebookDetails(
@@ -57,6 +74,8 @@ public class StartActivity extends FacebookActivity {
 												.getAccessToken(), getSession()
 												.getExpirationDate().getTime());
 								intent.putExtra("fb_details", details);
+								
+								Log.i(Constants.LOG_TAG, "Starting ChoosieActivity");
 								startActivity(intent);
 							}
 						}
