7cea79271010e93101db8072437a1694e8d67089
jonerez <jonerez@gmail.com>
2012-11-24 15:03:24 +0000

    Added basic pagination support, server side

--- choosie-server/homepage_handler.py
+++ choosie-server/homepage_handler.py
@@ -1,5 +1,6 @@
 import webapp2
 from module_choosie_post import ChoosiePost
+from feed_handler import FeedHandler
 from google.appengine.ext import db
 import jinja2
 import os
@@ -10,10 +11,15 @@ jinja_environment = jinja2.Environment(
 class HomepageHandler(webapp2.RequestHandler):
   def get(self):
     self.response.headers['Content-Type'] = 'text/html'
-
-    choosie_posts = db.GqlQuery("SELECT * FROM ChoosiePost ORDER BY created_at DESC LIMIT 10")
+    limit = self.request.get('limit')
+    if limit:
+      limit = int(limit)
+    choosie_posts, cursor = FeedHandler.GetFeedAndCursor(self.request.get('cursor'), limit)
     template_values = {
-      'choosie_posts': choosie_posts
+      'choosie_posts': choosie_posts,
+      'cursor': cursor,
+      'limit': limit
     }
+
     template = jinja_environment.get_template('index.html')
     self.response.write(template.render(template_values))
