995085bc7b1583bb71e8cfa218fe211d64001d18
jonerez@gmail.com <jonerez@gmail.com@6f50cedd-f6cd-5b0e-d468-1939c3eac22f>
2012-11-24 15:03:24 +0000

    Added basic pagination support, server side

--- trunk/choosie-server/feed_handler.py
+++ trunk/choosie-server/feed_handler.py
@@ -7,6 +7,21 @@ import logging
 from utils import Utils
 
 class FeedHandler(webapp2.RequestHandler):
-  def get(self):
-    choosie_posts = db.GqlQuery("SELECT * FROM ChoosiePost ORDER BY created_at DESC LIMIT 10")
-    self.response.out.write(json.dumps({"feed" : Utils.items_to_json(choosie_posts)}))
+	def get(self):
+		choosie_posts, cursor = FeedHandler.GetFeedAndCursor(self.request.get('cursor'), self.request.get('limit'))
+		choosie_posts_json = Utils.items_to_json(choosie_posts)
+		self.response.out.write(json.dumps({'feed' : choosie_posts_json, 'cursor': cursor}))
+		
+	@staticmethod
+	def GetFeedAndCursor(cursor, limit = 10):
+		if not limit:
+			limit = 10
+		posts = ChoosiePost.all()
+		if cursor:
+			posts.with_cursor(cursor)
+		posts.order("-created_at")
+		posts_result = []
+		for post in posts.run(limit=int(limit)):
+			posts_result.append(post)
+		new_cursor = posts.cursor()
+		return (posts_result, new_cursor)
