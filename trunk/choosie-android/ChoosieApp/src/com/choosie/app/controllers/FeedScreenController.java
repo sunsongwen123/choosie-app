93f0abe0dbfbd9d52492f0a0da5541a08ad95077
jonerez@gmail.com <jonerez@gmail.com@6f50cedd-f6cd-5b0e-d468-1939c3eac22f>
2012-11-30 20:24:32 +0000

    Cleaned up the look: no lines, white background

--- trunk/choosie-android/ChoosieApp/src/com/choosie/app/controllers/FeedScreenController.java
+++ trunk/choosie-android/ChoosieApp/src/com/choosie/app/controllers/FeedScreenController.java
@@ -82,7 +82,7 @@ public class FeedScreenController extends ScreenController {
 
 	public void refreshPost(ChoosiePostData post) {
 		choosiePostsItemAdapter.refreshItem(post);
-		Toast toast = Toast.makeText(getActivity(), "Refreshing post...",
+		Toast toast = Toast.makeText(getActivity(), "Post refreshed.",
 				Toast.LENGTH_SHORT);
 		toast.show();
 
