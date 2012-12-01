205f0412e0b1f46c20eec333f8deddb8a3c98c5b
klinger.idan@gmail.com <klinger.idan@gmail.com@6f50cedd-f6cd-5b0e-d468-1939c3eac22f>
2012-12-01 16:29:36 +0000

    Add permissions for: "publish_stream" "email"

--- trunk/choosie-android/ChoosieApp/src/com/choosie/app/StartActivity.java
+++ trunk/choosie-android/ChoosieApp/src/com/choosie/app/StartActivity.java
@@ -1,5 +1,8 @@
 package com.choosie.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.choosie.app.Models.FacebookDetails;
 import com.facebook.FacebookActivity;
 import com.facebook.GraphUser;
@@ -18,27 +21,41 @@ import android.widget.TextView;
 
 public class StartActivity extends FacebookActivity {
 
+	private String APP_ID;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start);
+
+		APP_ID = getResources().getString(R.string.app_id);
 
 		TextView welcome = (TextView) findViewById(R.id.welcome);
 		welcome.setText(getResources().getString(R.string.welcome) + " "
 				+ getResources().getString(R.string.app_name) + "!");
 
 		Button fbLoginButton = (Button) findViewById(R.id.fbLoginButton);
+
+		final List<String> permissions = new ArrayList<String>() {
+			private static final long serialVersionUID = 1L;
+			{
+				add("publish_stream");
+				add("email");
+			}
+		};
+
 		fbLoginButton.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View v) {
 				Log.i(Constants.LOG_TAG, "on click open session");
-				closeSession();
-				openSession();
+				// closeSession();
+
+				openSessionForPublish(APP_ID, permissions);
 
 			}
 		});
 
-		openSession();
+		openSessionForPublish(APP_ID, permissions);
 	}
 
 	@Override
