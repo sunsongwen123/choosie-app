ec89e8e57baab0587805ea6ed3095c89fcb6d74a
ShovalRon <shovalron@gmail.com>
2012-12-08 16:40:51 +0000

    adding post to fb

--- choosie-server/login_handler.py
+++ choosie-server/login_handler.py
@@ -2,16 +2,15 @@ from module_user import User
 from google.appengine.ext import db
 import webapp2
 from datetime import datetime
+from utils import Utils
 import logging
 import json
 
 class LoginHandler(webapp2.RequestHandler):
   def post(self):
-    bobo = str(self.request.get('fb_access_toke'))
     fb_access_token = str(self.request.get('fb_access_token'))
     fb_uid = str(self.request.get('fb_uid'))
-    fb_access_token_expdate = datetime.fromtimestamp(long(self.request.get('fb_access_token_expdate')) / 1e3)
-    # fb_access_token_expdate = fb_access_token_expdate.replace(second=0, microsecond=0)
+    fb_access_token_expdate = Utils.get_access_token_from_request(self.request)
     user = User.get_user_by_fb_uid(fb_uid)
     if not user:
       User.create(fb_access_token, fb_access_token_expdate)
--- choosie-server/utils.py
+++ choosie-server/utils.py
@@ -1,7 +1,10 @@
 from google.appengine.ext import db
 from google.appengine.api import urlfetch
-
-
+from datetime import datetime
+import urllib2
+import cStringIO
+from google.appengine.api import images
+import logging
 class Utils():
     @staticmethod
     def get_user_from_fb(fb_access_token):
@@ -17,5 +20,21 @@ class Utils():
       for item in items:
         arr.append(item.to_json())
       return arr
+
+    @staticmethod
+    def get_access_token_from_request(request):
+      return datetime.fromtimestamp(long(request.get('fb_access_token_expdate')) / 1e3)
+
+    @staticmethod
+    def create_post_image(choosie_post):
+      img1 = images.Image(choosie_post.photo1)
+      img1.resize(240,352)
+      img2 = images.Image(choosie_post.photo2)
+      img2.resize(240,352)
+      composite = images.composite([(img1, 0, 0, 1.0, images.TOP_LEFT),
+      (img2, 240, 0, 1.0, images.TOP_LEFT)], 480, 704)
+      choosie_post.photo = db.Blob(composite)
+      choosie_post.put()
+      logging.info('___saved')
 
     
