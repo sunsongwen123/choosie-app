532b5aebb1465f7f187e0d17f602733a5f1b7870
jonerez@gmail.com <jonerez@gmail.com@6f50cedd-f6cd-5b0e-d468-1939c3eac22f>
2012-12-15 19:48:31 +0000

    Unicode (and Hebrew) support in question / comments

--- trunk/choosie-server/comments_handler.py
+++ trunk/choosie-server/comments_handler.py
@@ -8,7 +8,9 @@ import logging
 class CommentsHandler(webapp2.RequestHandler):
   def post(self):
     fb_uid = str(self.request.get('fb_uid'))
-    text = str(self.request.get('text'))
+    # TODO: Make sure text is Unicode
+    text = self.request.get('text')
+
     # Since the post is taken from the cache, it might not be the most updated version
     # but that's ok, as it is only used as 'parent'
     choosie_post = CacheController.get_model(self.request.get('post_key'))
