bf179dc4e75c00ea140ace2d3de7706d4245639c
oren363 <oren363@gmail.com>
2013-01-11 16:21:29 +0000

    moving function blur and combine into utils

--- choosie-android/ChoosieApp/src/com/choosie/app/Utils.java
+++ choosie-android/ChoosieApp/src/com/choosie/app/Utils.java
@@ -19,8 +19,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Bitmap.CompressFormat;
+import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Debug;
 import android.os.Environment;
@@ -320,5 +324,240 @@ public class Utils {
 				Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
 		mediaScanIntent.setData(uri);
 		context.sendBroadcast(mediaScanIntent);
+	}
+
+	public static Bitmap combine(Bitmap bitmap1, Drawable drawable2) {
+		int width = bitmap1.getWidth();
+		int height = bitmap1.getHeight();
+
+		Bitmap bitmap = null;
+		try {
+			bitmap = Bitmap.createBitmap(width, height, Config.ARGB_8888);
+			Canvas c = new Canvas(bitmap);
+			// Resources res = getResources();
+
+			// Bitmap bitmap1 = BitmapFactory
+			// .decodeResource(res, R.drawable.test1); // blue
+
+			// Bitmap bitmap2 = BitmapFactory
+			// .decodeResource(res, R.drawable.test2); // green
+			Drawable drawable1 = new BitmapDrawable(bitmap1);
+			// Drawable drawable2 = new BitmapDrawable(bitmap2);
+
+			drawable1.setBounds(0, 0, width, height);
+			drawable2.setBounds(0, 0, width, height);
+			drawable1.draw(c);
+			drawable2.draw(c);
+
+		} catch (Exception e) {
+
+		}
+		return bitmap;
+	}
+
+	public static Bitmap fastblur(Bitmap sentBitmap, int radius) {
+		// Stack Blur Algorithm by Mario Klingemann <mario@quasimondo.com>
+
+		Bitmap bitmap = sentBitmap.copy(sentBitmap.getConfig(), true);
+
+		if (radius < 1) {
+			return (null);
+		}
+
+		int w = bitmap.getWidth();
+		int h = bitmap.getHeight();
+
+		int[] pix = new int[w * h];
+		Log.e("pix", w + " " + h + " " + pix.length);
+		bitmap.getPixels(pix, 0, w, 0, 0, w, h);
+
+		int wm = w - 1;
+		int hm = h - 1;
+		int wh = w * h;
+		int div = radius + radius + 1;
+
+		int r[] = new int[wh];
+		int g[] = new int[wh];
+		int b[] = new int[wh];
+		int rsum, gsum, bsum, x, y, i, p, yp, yi, yw;
+		int vmin[] = new int[Math.max(w, h)];
+
+		int divsum = (div + 1) >> 1;
+		divsum *= divsum;
+		int dv[] = new int[256 * divsum];
+		for (i = 0; i < 256 * divsum; i++) {
+			dv[i] = (i / divsum);
+		}
+
+		yw = yi = 0;
+
+		int[][] stack = new int[div][3];
+		int stackpointer;
+		int stackstart;
+		int[] sir;
+		int rbs;
+		int r1 = radius + 1;
+		int routsum, goutsum, boutsum;
+		int rinsum, ginsum, binsum;
+
+		for (y = 0; y < h; y++) {
+			rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
+			for (i = -radius; i <= radius; i++) {
+				p = pix[yi + Math.min(wm, Math.max(i, 0))];
+				sir = stack[i + radius];
+				sir[0] = (p & 0xff0000) >> 16;
+				sir[1] = (p & 0x00ff00) >> 8;
+				sir[2] = (p & 0x0000ff);
+				rbs = r1 - Math.abs(i);
+				rsum += sir[0] * rbs;
+				gsum += sir[1] * rbs;
+				bsum += sir[2] * rbs;
+				if (i > 0) {
+					rinsum += sir[0];
+					ginsum += sir[1];
+					binsum += sir[2];
+				} else {
+					routsum += sir[0];
+					goutsum += sir[1];
+					boutsum += sir[2];
+				}
+			}
+			stackpointer = radius;
+
+			for (x = 0; x < w; x++) {
+
+				r[yi] = dv[rsum];
+				g[yi] = dv[gsum];
+				b[yi] = dv[bsum];
+
+				rsum -= routsum;
+				gsum -= goutsum;
+				bsum -= boutsum;
+
+				stackstart = stackpointer - radius + div;
+				sir = stack[stackstart % div];
+
+				routsum -= sir[0];
+				goutsum -= sir[1];
+				boutsum -= sir[2];
+
+				if (y == 0) {
+					vmin[x] = Math.min(x + radius + 1, wm);
+				}
+				p = pix[yw + vmin[x]];
+
+				sir[0] = (p & 0xff0000) >> 16;
+				sir[1] = (p & 0x00ff00) >> 8;
+				sir[2] = (p & 0x0000ff);
+
+				rinsum += sir[0];
+				ginsum += sir[1];
+				binsum += sir[2];
+
+				rsum += rinsum;
+				gsum += ginsum;
+				bsum += binsum;
+
+				stackpointer = (stackpointer + 1) % div;
+				sir = stack[(stackpointer) % div];
+
+				routsum += sir[0];
+				goutsum += sir[1];
+				boutsum += sir[2];
+
+				rinsum -= sir[0];
+				ginsum -= sir[1];
+				binsum -= sir[2];
+
+				yi++;
+			}
+			yw += w;
+		}
+		for (x = 0; x < w; x++) {
+			rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
+			yp = -radius * w;
+			for (i = -radius; i <= radius; i++) {
+				yi = Math.max(0, yp) + x;
+
+				sir = stack[i + radius];
+
+				sir[0] = r[yi];
+				sir[1] = g[yi];
+				sir[2] = b[yi];
+
+				rbs = r1 - Math.abs(i);
+
+				rsum += r[yi] * rbs;
+				gsum += g[yi] * rbs;
+				bsum += b[yi] * rbs;
+
+				if (i > 0) {
+					rinsum += sir[0];
+					ginsum += sir[1];
+					binsum += sir[2];
+				} else {
+					routsum += sir[0];
+					goutsum += sir[1];
+					boutsum += sir[2];
+				}
+
+				if (i < hm) {
+					yp += w;
+				}
+			}
+			yi = x;
+			stackpointer = radius;
+			for (y = 0; y < h; y++) {
+				// Preserve alpha channel: ( 0xff000000 & pix[yi] )
+				pix[yi] = (0xff000000 & pix[yi]) | (dv[rsum] << 16)
+						| (dv[gsum] << 8) | dv[bsum];
+
+				rsum -= routsum;
+				gsum -= goutsum;
+				bsum -= boutsum;
+
+				stackstart = stackpointer - radius + div;
+				sir = stack[stackstart % div];
+
+				routsum -= sir[0];
+				goutsum -= sir[1];
+				boutsum -= sir[2];
+
+				if (x == 0) {
+					vmin[y] = Math.min(y + r1, hm) * w;
+				}
+				p = x + vmin[y];
+
+				sir[0] = r[p];
+				sir[1] = g[p];
+				sir[2] = b[p];
+
+				rinsum += sir[0];
+				ginsum += sir[1];
+				binsum += sir[2];
+
+				rsum += rinsum;
+				gsum += ginsum;
+				bsum += binsum;
+
+				stackpointer = (stackpointer + 1) % div;
+				sir = stack[stackpointer];
+
+				routsum += sir[0];
+				goutsum += sir[1];
+				boutsum += sir[2];
+
+				rinsum -= sir[0];
+				ginsum -= sir[1];
+				binsum -= sir[2];
+
+				yi += w;
+			}
+		}
+
+		Log.e("pix", w + " " + h + " " + pix.length);
+		bitmap.setPixels(pix, 0, w, 0, 0, w, h);
+
+		return (bitmap);
 	}
 }
--- choosie-android/ChoosieApp/src/com/choosie/app/camera/CameraMainSuperControllerActivity.java
+++ choosie-android/ChoosieApp/src/com/choosie/app/camera/CameraMainSuperControllerActivity.java
@@ -24,6 +24,8 @@ import com.google.analytics.tracking.android.Tracker;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
@@ -161,32 +163,8 @@ public class CameraMainSuperControllerActivity extends Activity {
 					return true;
 				}
 
-				int maxX = arg0.getWidth();
-				int maxY = arg0.getHeight();
-
-				float x = arg1.getX();
-				float y = arg1.getY();
-
-				// if moved outside the button - make the focus image gone
-				if (arg1.getAction() == MotionEvent.ACTION_MOVE) {
-					if ((x < 0) || (y < 0) || (x > maxX) || (y > maxY)) {
-						yaanaaImageView.setImageResource(R.drawable.yaa_naa);
-
-						return true;
-					}
-				}
-
-				if (arg1.getAction() == MotionEvent.ACTION_DOWN) {
-					yaanaaImageView
-							.setImageResource(R.drawable.yaa_naa_pressed);
-				}
-
-				if (arg1.getAction() == MotionEvent.ACTION_UP) {
-
-					if ((x > 0) && (y > 0) && (x < maxX) && (y < maxY)) {
-						yaanaaImageView.performClick();
-					}
-				}
+				handleOnTouch(yaanaaImageView, arg1, R.drawable.yaa_naa,
+						R.drawable.yaa_naa_pressed);
 
 				return true;
 
@@ -201,31 +179,8 @@ public class CameraMainSuperControllerActivity extends Activity {
 					return true;
 				}
 
-				int maxX = arg0.getWidth();
-				int maxY = arg0.getHeight();
-
-				float x = arg1.getX();
-				float y = arg1.getY();
-
-				// if moved outside the button - make the focus image gone
-				if (arg1.getAction() == MotionEvent.ACTION_MOVE) {
-					if ((x < 0) || (y < 0) || (x > maxX) || (y > maxY)) {
-						totImageView.setImageResource(R.drawable.tot);
-
-						return true;
-					}
-				}
-
-				if (arg1.getAction() == MotionEvent.ACTION_DOWN) {
-					totImageView.setImageResource(R.drawable.tot_pressed);
-				}
-
-				if (arg1.getAction() == MotionEvent.ACTION_UP) {
-
-					if ((x > 0) && (y > 0) && (x < maxX) && (y < maxY)) {
-						totImageView.performClick();
-					}
-				}
+				handleOnTouch(totImageView, arg1, R.drawable.tot,
+						R.drawable.tot_pressed);
 
 				return true;
 
@@ -253,6 +208,38 @@ public class CameraMainSuperControllerActivity extends Activity {
 
 			}
 		});
+	}
+
+	protected void handleOnTouch(ImageView view, MotionEvent motion,
+			int drawable, int drawablePressed) {
+		int maxX = view.getWidth();
+		int maxY = view.getHeight();
+
+		float x = motion.getX();
+		float y = motion.getY();
+
+		// if moved outside the button - make the focus image gone
+		if (motion.getAction() == MotionEvent.ACTION_MOVE) {
+			if ((x < 0) || (y < 0) || (x > maxX) || (y > maxY)) {
+				view.setImageResource(drawable);
+				return;
+			}
+		}
+
+		if (motion.getAction() == MotionEvent.ACTION_DOWN) {
+			view.setImageResource(drawablePressed);
+		}
+
+		if (motion.getAction() == MotionEvent.ACTION_UP) {
+
+			if ((x > 0) && (y > 0) && (x < maxX) && (y < maxY)) {
+				if (view.getId() == R.id.post_yaanaaButton_image) {
+					yaanaaImageView.performClick();
+				} else if (view.getId() == R.id.post_totButton_image) {
+					totImageView.performClick();
+				}
+			}
+		}
 	}
 
 	protected void handleImage1Click() {
@@ -360,9 +347,9 @@ public class CameraMainSuperControllerActivity extends Activity {
 				R.anim.fadeout);
 		fadeOutAnimation.setAnimationListener(new AnimationListener() {
 
-			public void onAnimationStart(Animation animation) {
-				// TODO Auto-generated method stub
-
+			public void onAnimationEnd(Animation animation) {
+				mImage2.setImageBitmap(null);
+				mImage2.startAnimation(fadeInAnimation);
 			}
 
 			public void onAnimationRepeat(Animation animation) {
@@ -370,41 +357,14 @@ public class CameraMainSuperControllerActivity extends Activity {
 
 			}
 
-			public void onAnimationEnd(Animation animation) {
-				mImage2.setImageBitmap(null);
-				mImage2.startAnimation(fadeInAnimation);
+			public void onAnimationStart(Animation animation) {
+				// TODO Auto-generated method stub
+
 			}
 		});
 
 		mImage2.startAnimation(fadeOutAnimation);
-		// mImage1.startAnimation(fadeInAnimation);
-
 		mImage1.setImageBitmap(image1BitmapYaanaa);
-		// mImage2.setAnimation(null);
-
-		// Animation fadeInAnimation = AnimationUtils.loadAnimation(this,
-		// R.anim.push_right_in);
-		// fadeInAnimation.setAnimationListener(new AnimationListener() {
-		//
-		// public void onAnimationStart(Animation animation) {
-		//
-		// mImage2.setImageBitmap(image2BitmapYaanaa);
-		//
-		// }
-		//
-		// public void onAnimationRepeat(Animation animation) {
-		// // TODO Auto-generated method stub
-		//
-		// }
-		//
-		// public void onAnimationEnd(Animation animation) {
-		// mImage2.setImageBitmap(image2BitmapYaanaa);
-		//
-		// }
-		// });
-		// mImage2.setAnimation(fadeInAnimation);
-		// fadeInAnimation.startNow();
-		// mImage2.startAnimation(fadeInAnimation);
 
 		postType = PostType.YesNo;
 	}
@@ -456,7 +416,6 @@ public class CameraMainSuperControllerActivity extends Activity {
 				saveImage1Bitmap();
 				setImages();
 
-				// startSecondPicDialog();
 			} else if (resultCode == Activity.RESULT_CANCELED) {
 				// user declined, let's start the CameraActivity again with
 				// first image
@@ -470,7 +429,6 @@ public class CameraMainSuperControllerActivity extends Activity {
 			if (resultCode == Activity.RESULT_OK) {
 				// user confirmed. starting NewPostActivity
 				Utils.galleryAddPic(Uri.fromFile(imageFile2), this);
-
 				saveImage2Bitmap();
 				mImage2.setBackgroundDrawable(null);
 				mImage2.setImageBitmap(image2BitmapTot);
@@ -551,21 +509,10 @@ public class CameraMainSuperControllerActivity extends Activity {
 
 		// set image 1 and 2 yaanaa
 
-		image1BitmapYaanaa = combine(image1BitmapTot, getResources()
+		image1BitmapYaanaa = Utils.combine(image1BitmapTot, getResources()
 				.getDrawable(R.drawable.yaa));
-		image2BitmapYaanaa = combine(fastblur(image1BitmapTot, 20),
+		image2BitmapYaanaa = Utils.combine(Utils.fastblur(image1BitmapTot, 20),
 				getResources().getDrawable(R.drawable.naa));
-	}
-
-	private void handleResultFromCamera(int resultCode, Intent data,
-			int requestCode, String path) {
-		if (resultCode == Activity.RESULT_OK) {
-			// starting ConfirmationActivity
-			bundle = data.getExtras();
-			startConfirmActivity(requestCode, path);
-		} else if (resultCode == Activity.RESULT_CANCELED) {
-			goBackToChoosieActivity(Activity.RESULT_CANCELED);
-		}
 	}
 
 	private void goBackToChoosieActivity(int result) {
@@ -603,274 +550,12 @@ public class CameraMainSuperControllerActivity extends Activity {
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if (keyCode == KeyEvent.KEYCODE_BACK) {
+
 			setResult(Activity.RESULT_CANCELED);
 			this.finish();
 			return true;
 		}
 		return super.onKeyDown(keyCode, event);
-	}
-
-	public Bitmap fastblur(Bitmap sentBitmap, int radius) {
-
-		// Stack Blur v1.0 from
-		// http://www.quasimondo.com/StackBlurForCanvas/StackBlurDemo.html
-		//
-		// Java Author: Mario Klingemann <mario at quasimondo.com>
-		// http://incubator.quasimondo.com
-		// created Feburary 29, 2004
-		// Android port : Yahel Bouaziz <yahel at kayenko.com>
-		// http://www.kayenko.com
-		// ported april 5th, 2012
-
-		// This is a compromise between Gaussian Blur and Box blur
-		// It creates much better looking blurs than Box Blur, but is
-		// 7x faster than my Gaussian Blur implementation.
-		//
-		// I called it Stack Blur because this describes best how this
-		// filter works internally: it creates a kind of moving stack
-		// of colors whilst scanning through the image. Thereby it
-		// just has to add one new block of color to the right side
-		// of the stack and remove the leftmost color. The remaining
-		// colors on the topmost layer of the stack are either added on
-		// or reduced by one, depending on if they are on the right or
-		// on the left side of the stack.
-		//
-		// If you are using this algorithm in your code please add
-		// the following line:
-		//
-		// Stack Blur Algorithm by Mario Klingemann <mario@quasimondo.com>
-
-		Bitmap bitmap = sentBitmap.copy(sentBitmap.getConfig(), true);
-
-		if (radius < 1) {
-			return (null);
-		}
-
-		int w = bitmap.getWidth();
-		int h = bitmap.getHeight();
-
-		int[] pix = new int[w * h];
-		Log.e("pix", w + " " + h + " " + pix.length);
-		bitmap.getPixels(pix, 0, w, 0, 0, w, h);
-
-		int wm = w - 1;
-		int hm = h - 1;
-		int wh = w * h;
-		int div = radius + radius + 1;
-
-		int r[] = new int[wh];
-		int g[] = new int[wh];
-		int b[] = new int[wh];
-		int rsum, gsum, bsum, x, y, i, p, yp, yi, yw;
-		int vmin[] = new int[Math.max(w, h)];
-
-		int divsum = (div + 1) >> 1;
-		divsum *= divsum;
-		int dv[] = new int[256 * divsum];
-		for (i = 0; i < 256 * divsum; i++) {
-			dv[i] = (i / divsum);
-		}
-
-		yw = yi = 0;
-
-		int[][] stack = new int[div][3];
-		int stackpointer;
-		int stackstart;
-		int[] sir;
-		int rbs;
-		int r1 = radius + 1;
-		int routsum, goutsum, boutsum;
-		int rinsum, ginsum, binsum;
-
-		for (y = 0; y < h; y++) {
-			rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
-			for (i = -radius; i <= radius; i++) {
-				p = pix[yi + Math.min(wm, Math.max(i, 0))];
-				sir = stack[i + radius];
-				sir[0] = (p & 0xff0000) >> 16;
-				sir[1] = (p & 0x00ff00) >> 8;
-				sir[2] = (p & 0x0000ff);
-				rbs = r1 - Math.abs(i);
-				rsum += sir[0] * rbs;
-				gsum += sir[1] * rbs;
-				bsum += sir[2] * rbs;
-				if (i > 0) {
-					rinsum += sir[0];
-					ginsum += sir[1];
-					binsum += sir[2];
-				} else {
-					routsum += sir[0];
-					goutsum += sir[1];
-					boutsum += sir[2];
-				}
-			}
-			stackpointer = radius;
-
-			for (x = 0; x < w; x++) {
-
-				r[yi] = dv[rsum];
-				g[yi] = dv[gsum];
-				b[yi] = dv[bsum];
-
-				rsum -= routsum;
-				gsum -= goutsum;
-				bsum -= boutsum;
-
-				stackstart = stackpointer - radius + div;
-				sir = stack[stackstart % div];
-
-				routsum -= sir[0];
-				goutsum -= sir[1];
-				boutsum -= sir[2];
-
-				if (y == 0) {
-					vmin[x] = Math.min(x + radius + 1, wm);
-				}
-				p = pix[yw + vmin[x]];
-
-				sir[0] = (p & 0xff0000) >> 16;
-				sir[1] = (p & 0x00ff00) >> 8;
-				sir[2] = (p & 0x0000ff);
-
-				rinsum += sir[0];
-				ginsum += sir[1];
-				binsum += sir[2];
-
-				rsum += rinsum;
-				gsum += ginsum;
-				bsum += binsum;
-
-				stackpointer = (stackpointer + 1) % div;
-				sir = stack[(stackpointer) % div];
-
-				routsum += sir[0];
-				goutsum += sir[1];
-				boutsum += sir[2];
-
-				rinsum -= sir[0];
-				ginsum -= sir[1];
-				binsum -= sir[2];
-
-				yi++;
-			}
-			yw += w;
-		}
-		for (x = 0; x < w; x++) {
-			rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
-			yp = -radius * w;
-			for (i = -radius; i <= radius; i++) {
-				yi = Math.max(0, yp) + x;
-
-				sir = stack[i + radius];
-
-				sir[0] = r[yi];
-				sir[1] = g[yi];
-				sir[2] = b[yi];
-
-				rbs = r1 - Math.abs(i);
-
-				rsum += r[yi] * rbs;
-				gsum += g[yi] * rbs;
-				bsum += b[yi] * rbs;
-
-				if (i > 0) {
-					rinsum += sir[0];
-					ginsum += sir[1];
-					binsum += sir[2];
-				} else {
-					routsum += sir[0];
-					goutsum += sir[1];
-					boutsum += sir[2];
-				}
-
-				if (i < hm) {
-					yp += w;
-				}
-			}
-			yi = x;
-			stackpointer = radius;
-			for (y = 0; y < h; y++) {
-				// Preserve alpha channel: ( 0xff000000 & pix[yi] )
-				pix[yi] = (0xff000000 & pix[yi]) | (dv[rsum] << 16)
-						| (dv[gsum] << 8) | dv[bsum];
-
-				rsum -= routsum;
-				gsum -= goutsum;
-				bsum -= boutsum;
-
-				stackstart = stackpointer - radius + div;
-				sir = stack[stackstart % div];
-
-				routsum -= sir[0];
-				goutsum -= sir[1];
-				boutsum -= sir[2];
-
-				if (x == 0) {
-					vmin[y] = Math.min(y + r1, hm) * w;
-				}
-				p = x + vmin[y];
-
-				sir[0] = r[p];
-				sir[1] = g[p];
-				sir[2] = b[p];
-
-				rinsum += sir[0];
-				ginsum += sir[1];
-				binsum += sir[2];
-
-				rsum += rinsum;
-				gsum += ginsum;
-				bsum += binsum;
-
-				stackpointer = (stackpointer + 1) % div;
-				sir = stack[stackpointer];
-
-				routsum += sir[0];
-				goutsum += sir[1];
-				boutsum += sir[2];
-
-				rinsum -= sir[0];
-				ginsum -= sir[1];
-				binsum -= sir[2];
-
-				yi += w;
-			}
-		}
-
-		Log.e("pix", w + " " + h + " " + pix.length);
-		bitmap.setPixels(pix, 0, w, 0, 0, w, h);
-
-		return (bitmap);
-	}
-
-	private Bitmap combine(Bitmap bitmap1, Drawable drawable2) {
-
-		int width = bitmap1.getWidth();
-		int height = bitmap1.getHeight();
-
-		Bitmap bitmap = null;
-		try {
-			bitmap = Bitmap.createBitmap(width, height, Config.ARGB_8888);
-			Canvas c = new Canvas(bitmap);
-			// Resources res = getResources();
-
-			// Bitmap bitmap1 = BitmapFactory
-			// .decodeResource(res, R.drawable.test1); // blue
-
-			// Bitmap bitmap2 = BitmapFactory
-			// .decodeResource(res, R.drawable.test2); // green
-			Drawable drawable1 = new BitmapDrawable(bitmap1);
-			// Drawable drawable2 = new BitmapDrawable(bitmap2);
-
-			drawable1.setBounds(0, 0, width, height);
-			drawable2.setBounds(0, 0, width, height);
-			drawable1.draw(c);
-			drawable2.draw(c);
-
-		} catch (Exception e) {
-
-		}
-		return bitmap;
 	}
 
 	private OnCheckedChangeListener checkChangedListener = new OnCheckedChangeListener() {
@@ -1030,4 +715,21 @@ public class CameraMainSuperControllerActivity extends Activity {
 		}
 		return true;
 	}
+
+	// private void showErrorDialog(String errorMsg) {
+	// AlertDialog.Builder myAlertDialog = new AlertDialog.Builder(this,
+	// R.style.);
+	// myAlertDialog.setTitle("");
+	// myAlertDialog.setMessage(errorMsg);
+	//
+	// myAlertDialog.setPositiveButton("OK",
+	// new DialogInterface.OnClickListener() {
+	// public void onClick(DialogInterface arg1, int arg3) {
+	// setResult(Activity.RESULT_CANCELED);
+	// finish();
+	// }
+	// });
+	// myAlertDialog.show();
+
+	// }
 }
